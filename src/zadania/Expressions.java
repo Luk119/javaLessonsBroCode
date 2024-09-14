18e1cbdeae53e174416229bbe9075a00867db8bb
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-14 16:45:18 +0200

    expressions

---
+++ src/zadania/Expressions.java
@@ -0,0 +1,20 @@
+package zadania;
+
+public class Expressions {
+    public static void main(String[] args){
+
+        int x = 23;
+
+        System.out.println("x: " + x);
+        x++;
+        System.out.println("x: " + x);
+        --x;
+        System.out.println("x: " + x--);
+        System.out.println("x: " + x);
+        x+=1;
+
+        System.out.println("x/2 = " + x/2);
+
+        System.out.println("x/2 = " + (float)x/2);
+    }
+}
