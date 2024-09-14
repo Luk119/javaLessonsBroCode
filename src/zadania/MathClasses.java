c7edb6fc66a2cfc07edbdc624624dcc989616be0
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-14 17:03:42 +0200

    Math classes

---
+++ src/zadania/MathClasses.java
@@ -0,0 +1,16 @@
+package zadania;
+
+public class MathClasses {
+    public static void main(String[] args){
+
+        float x = 3.14f;
+        float y = 10f;
+
+        System.out.println("pow(a): " + Math.pow(y, 3));
+        System.out.println("sqrt(a): " + Math.sqrt(y));
+        System.out.println("round(a): " + Math.round(x));
+        System.out.println("ceil(a): " + Math.ceil(x));
+        System.out.println("floor(a): " + Math.floor(x));
+
+    }
+}
