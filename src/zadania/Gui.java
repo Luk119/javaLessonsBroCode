a0cc2a1190eb252b4d40695d5e350e749089b4c8
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-14 16:56:28 +0200

    GUI introduction

---
+++ src/zadania/Gui.java
@@ -0,0 +1,15 @@
+package zadania;
+import javax.swing.*;
+
+public class Gui {
+    public static void main(String[] args){
+
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+        JOptionPane.showMessageDialog(null, "Hi " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age: "));
+        JOptionPane.showMessageDialog(null, "You're " + age + " years old.");
+
+        System.out.println("Thanks for coming " + name);
+    }
+}
