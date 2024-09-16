d40aba550f8fa69a15c01ecdc394abc4dc2acb14
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-16 15:15:51 +0200

    abs

--- src/zadania/MathClasses.java
+++ src/zadania/MathClasses.java
@@ -6,7 +6,8 @@ public class MathClasses {
         float x = 3.14f;
         float y = 10f;
 
-        System.out.println("pow(a): " + Math.pow(y, 3));
+        System.out.println("abs(a): " + Math.abs(-10));//absolut value
+        System.out.println("pow(a): " + Math.pow(y, 3));//power
         System.out.println("sqrt(a): " + Math.sqrt(y));
         System.out.println("round(a): " + Math.round(x));
         System.out.println("ceil(a): " + Math.ceil(x));
