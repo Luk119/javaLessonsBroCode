0cfcd6c0cee76eb0feb8331aad2242600b291646
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-16 17:24:03 +0200

    for loop (nested)

--- src/BroCodeLessons/ForLoop.java
+++ src/BroCodeLessons/ForLoop.java
@@ -1,11 +1,26 @@
 package BroCodeLessons;
+import java.util.Scanner;
 
 public class ForLoop {
     public static void main(String[] args){
 
-        for(int i = 0 ; i < 10 ; i++){
-            for(int j = 0 ; j < i ; j++){
-                System.out.print("@");
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int col;
+        String symbol = "";
+
+        System.out.println("enter rows: ");
+        rows = scanner.nextInt();
+        System.out.println("enter columns: ");
+        col = scanner.nextInt();
+        System.out.println("enter symbol: ");
+        symbol = scanner.next();
+
+
+        for(int i=0 ; i < rows ; i++){
+            for(int j = 0 ; j < col ; j++){
+                System.out.print(symbol);
             }
             System.out.println();
         }
