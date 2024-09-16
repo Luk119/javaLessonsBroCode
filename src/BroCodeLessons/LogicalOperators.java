834168e531017af55558c3cdccb9563eed5e512a
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-16 16:06:28 +0200

    Logical operators

---
+++ src/BroCodeLessons/LogicalOperators.java
@@ -0,0 +1,27 @@
+package BroCodeLessons;
+
+public class LogicalOperators {
+    public static void main(String[] args){
+        // && (AND gate)
+        // || (OR gate)
+        // ! (NOT gate)
+        int engineOil_temperature = 80;
+        int transmissionOil_temperature = 80;
+        boolean drivers_focus = false;
+
+        String command;
+
+        if(engineOil_temperature >= 80 && transmissionOil_temperature > 75 && drivers_focus == true){
+            command = "car: ready\ndriver: ready";
+        }
+        else if(engineOil_temperature >= 80 && transmissionOil_temperature > 75 && drivers_focus == false){
+            command = "car: ready\ndriver: NOT ready";
+        }
+        else{
+            command = "car: NOT ready\ndriver: NOT ready";
+        }
+
+        System.out.print("No. 19\n" + command);
+
+    }
+}
