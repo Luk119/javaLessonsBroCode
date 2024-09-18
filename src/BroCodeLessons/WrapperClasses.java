5371bf60ff7e973061157d8934315e61445265e0
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-18 12:05:31 +0200

    wrapper classes

---
+++ src/BroCodeLessons/WrapperClasses.java
@@ -0,0 +1,12 @@
+package BroCodeLessons;
+
+public class WrapperClasses {
+    public static void main(String[] args){
+
+        Boolean a = true;
+        Character b = '#';
+        Integer c = 1919;
+        Double d = 2.32;
+        String e = "String";
+    }
+}
