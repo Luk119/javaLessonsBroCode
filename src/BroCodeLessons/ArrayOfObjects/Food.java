113a31452f6c16e0fc1a178ea7b4d1d5d3c19221
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-19 17:11:58 +0200

    array of objects

---
+++ src/BroCodeLessons/ArrayOfObjects/Food.java
@@ -0,0 +1,16 @@
+package BroCodeLessons.ArrayOfObjects;
+
+public class Food {
+
+    String name;
+
+    Food(String name){
+
+        this.name = name;
+    }
+
+    public String toString(){
+        return name;
+    }
+}
+
