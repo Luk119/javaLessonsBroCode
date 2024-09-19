2b68465050bd240bb66d74b6385249b6c0efa887
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-19 16:14:33 +0200

    constructor overload/2

--- src/BroCodeLessons/OverloadedConstructors/Main.java
+++ src/BroCodeLessons/OverloadedConstructors/Main.java
@@ -8,10 +8,17 @@ public class Main {
         Pizza pizza1 = new Pizza("thick crust", "tomato", "mozzarella", "pepperoni");
         Pizza pizza2 = new Pizza("thin crust", "", "gouda");
 
-        System.out.println("Here are ingredients of your Pizza: ");
+        System.out.println("Here are ingredients of your pizza1: ");
         System.out.println(pizza1.bread);
         System.out.println(pizza1.sauce);
         System.out.println(pizza1.cheese);
         System.out.println(pizza1.topping);
+
+        System.out.println("Here are ingredients of your pizza2: ");
+        System.out.println(pizza2.bread);
+        System.out.println(pizza2.sauce);
+        System.out.println(pizza2.cheese);
+        System.out.println(pizza2.topping);
+
     }
 }
