86adedde046e45b718f9c83844de8f47ef93e520
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-19 16:56:00 +0200

    toString

---
+++ src/BroCodeLessons/ToString/Car.java
@@ -0,0 +1,14 @@
+package BroCodeLessons.ToString;
+
+public class Car {
+
+    String brand = "Ford";
+    String model = "mustang";
+    String color = "blue";
+    int year = 1988;
+
+    public String toString(){
+
+        return brand + "\n" + model + "\n" + color + "\n" + year;
+    }
+}
---
+++ src/BroCodeLessons/ToString/Main.java
@@ -0,0 +1,16 @@
+package BroCodeLessons.ToString;
+
+public class Main {
+    public static void main(String[] args){
+
+        Car car = new Car();
+
+//        System.out.println(car.brand);
+//        System.out.println(car.model);
+//        System.out.println(car.color);
+//        System.out.println(car.year);
+
+        System.out.println(car.toString()+ "\n");
+        System.out.println(car);
+    }
+}
