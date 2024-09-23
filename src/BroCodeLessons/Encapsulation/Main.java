92900f45df8242aa21cd70dd6e5baff51b98e76b
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-23 19:13:50 +0200

    Encapsulation

---
+++ src/BroCodeLessons/Encapsulation/Main.java
@@ -0,0 +1,19 @@
+package BroCodeLessons.Encapsulation;
+
+public class Main {
+    public static void main(String[] args){
+
+        Car car = new Car("Honda", "Civic", 2022);
+        System.out.println(car.getBrand());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
+
+        car.setBrand("Mazda");
+        car.setModel("CX-5");
+        car.setYear(2024);
+
+        System.out.println(car.getBrand());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
+    }
+}
