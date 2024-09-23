1ffd77b573e2aa2f68830e50c45010699416b345
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-23 19:06:30 +0200

    bug

--- src/BroCodeLessons/Package1/A.java
+++ src/BroCodeLessons/Package1/A.java
@@ -4,7 +4,7 @@ public class A{
     public static void main(String[] args){
 
         B b = new B();
-        System.out.println(b.privateMessage);// no access to B private message
+//        System.out.println(b.privateMessage); no access to B private message
     }
 
 
