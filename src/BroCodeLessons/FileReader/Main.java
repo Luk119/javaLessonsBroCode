ca909ab76c1536c7a96a55133cf8159dab1f25f8
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-25 22:22:05 +0200

    File writer

---
+++ src/BroCodeLessons/FileReader/Main.java
@@ -0,0 +1,21 @@
+package BroCodeLessons.FileReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args){
+
+        try{
+            FileWriter writer = new FileWriter( "src" + File.separator + "BroCodeLessons" + File.separator +
+                    "FileReader" + File.separator + "poem.txt");
+
+            writer.write("Roses are red\nViolets are blue");
+            writer.write("\n(A poem by B)");
+            writer.close();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
