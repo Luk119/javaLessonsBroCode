f23579367917efd78dd876a8c2a6f4f548cb9633
Luk119 <lukasz.kundzicz19@interia.pl>
2024-09-26 14:44:13 +0200

    Serialization

---
+++ src/BroCodeLessons/Serialization/Main.java
@@ -0,0 +1,28 @@
+package BroCodeLessons.Serialization;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        User user = new User();
+
+        user.name = "Łukasz";
+        user.password = "qwerty123";
+
+        try{
+
+            FileOutputStream fileOut = new FileOutputStream("src" + File.separator + "BroCodeLessons" + File.separator +
+                    "Serialization" + File.separator + "UserInfo.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(user);
+            out.close();
+            System.out.println("Object info saved!");
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
