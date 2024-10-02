ee34abd9b84c7f9b45b853b194de6d5c0dde00fd
Luk119 <lukasz.kundzicz19@interia.pl>
2024-10-02 10:48:32 +0200

    Multithreading

--- src/BroCodeLessons/ThreadsLearning/MyThread.java
+++ src/BroCodeLessons/ThreadsLearning/MyThread.java
@@ -5,11 +5,15 @@ public class MyThread extends Thread{
     @Override
     public void run(){
 
-        if(this.isDaemon()) {
-            System.out.println("This is a daemon thread that is running");
+        for(int i=10; i>0; i--){
+            System.out.println("Thread #1 : " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+//            System.out.println(1/0);
         }
-        else{
-            System.out.println("This is a user thread that is running");
-        }
+        System.out.println("Thread #1 is finished");
     }
 }
